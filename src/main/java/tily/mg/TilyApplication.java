8e3cc8aaaf7f86d14c240400bc836887d353dfab
Harena Andriamahatony <harena.andriamahatony@gmail.com>
2025-11-27 20:12:00 +0300

    intial commit

---
+++ src/main/java/tily/mg/TilyApplication.java
@@ -0,0 +1,13 @@
+package tily.mg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilyApplication.class, args);
+	}
+
+}
